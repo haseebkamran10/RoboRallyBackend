ac4b2d95117c7bc03b788e606a90e8ed08361ca8
haseebkamran10 <s224270@dtu.dk>
2024-06-14 15:13:00 +0200

    Now we can create Boards and Players by sending POST requests. Endpoints working

--- src/main/java/dk/dtu/compute/se/pisd/roborally/RoboRallyApplication.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/RoboRallyApplication.java
@@ -2,16 +2,45 @@ package dk.dtu.compute.se.pisd.roborally;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
-@EntityScan(basePackages = "dk.dtu.compute.se.pisd.roborally.api.model")
-@EnableJpaRepositories(basePackages = "dk.dtu.compute.se.pisd.roborally.api.repository")
-@ComponentScan(basePackages = "dk.dtu.compute.se.pisd.roborally")
 public class RoboRallyApplication {
+
 	public static void main(String[] args) {
-		SpringApplication.run(RoboRallyApplication.class, args);
+		SpringApplication app = new SpringApplication(RoboRallyApplication.class);
+		ConfigurableApplicationContext context = app.run(args);
+
+		System.out.println("Application has started");
+
+		// Keep the context open
+		synchronized (context) {
+			try {
+				context.wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@Bean
+	public ApplicationLifecycleListener applicationLifecycleListener() {
+		return new ApplicationLifecycleListener();
+	}
+
+	public static class ApplicationLifecycleListener {
+		@EventListener
+		public void onContextRefreshed(ContextRefreshedEvent event) {
+			System.out.println("Context refreshed: " + event);
+		}
+
+		@EventListener
+		public void onContextClosed(ContextClosedEvent event) {
+			System.out.println("Context closed: " + event);
+		}
 	}
 }
--- src/main/java/dk/dtu/compute/se/pisd/roborally/api/model/Board.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/api/model/Board.java
@@ -1,11 +1,9 @@
 package dk.dtu.compute.se.pisd.roborally.api.model;
 
-import com.google.gson.annotations.Expose;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.jetbrains.annotations.NotNull;
-import org.springframework.stereotype.Component;
+
 
 import java.util.ArrayList;
 import java.util.Arrays;
